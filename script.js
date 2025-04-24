d52be0b33e4b4e15fb593d58537c7fbbd1833ac8
ori61956 <ori.sadowsky1010@gmail.com>
2025-04-24 17:21:59 +0300

    updated logic and reisizeablity

---
+++ script.js
@@ -0,0 +1,45 @@
+const burger = document.getElementById("header_burger_menu");
+const nav = document.querySelector("nav");
+
+
+/* Base classes I want to use in order to enchance preformance:
+    menu-open: detect wether the menu has opened
+    is-desktop: checking wether the web is used in a desktop width
+
+    in the future:
+    dark mode 
+*/
+
+
+function declareStates() {
+    document.body.classList.toggle("menu-open", false);
+    document.body.classList.toggle("is-desktop", false);
+}
+
+function listenToDropDown(){
+   if(document.body.classList("is-desktop") === false){
+     burger.addEventListener("click", () => {
+        document.body.classList.toggle("menu-open", true);
+     })
+   } 
+}
+
+
+declareStates();
+listenToDropDown();
+
+/*
+    // add functionality for the header burger menu
+burger.addEventListener("mousedown", () => {
+    nav.classList.toggle("open", true);
+});
+
+burger.addEventListener("mouseup", () => {
+    nav.classList.toggle("open", false);
+});
+
+
+
+*/
+
+
